cc3284fb5368f5185f29ffeec19752cdb71e322d
Toby McDonald <toby_mcdonald@email.com>
2022-02-10 19:23:43 -0500

    Implemented Patient List & Triage Indicator

--- ems.php
+++ ems.php
@@ -6,8 +6,9 @@ echo"
 	<div class='col-sm-5'>
 		<div class='panel panel-info'>
 			<div class='panel-heading'>Patients</div>
-			<div class='panel-body'>
-				<table class='table table-striped table-hover'>
+			<div class='panel-body'>";
+			if ($_SESSION["incident"] != null) {
+				echo"<table class='table table-striped table-hover'>
 					<thead>
 						<tr>
 							<th>Name</th>
@@ -15,35 +16,95 @@ echo"
 							<th>Status</th>
 						</tr>
 					</thead>
-					<tbody>
-						<tr>
-							<td><span class='label label-danger'>Immediate</span> John Doe (35M)</td>
-							<td>Major Hemorrhage</td>
-							<td>Transported</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+					<tbody>";
+						$conn = new mysqli($db_server, $db_user, $db_password, $db_db);
+						$sql = "SELECT * FROM patients WHERE Incident = ".$_SESSION["incident"];
+						$result = $conn->query($sql);
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								$triagecolor = "label-primary";
+								$triagename = "Unknown";
+								switch ($row["triage"]) {
+									case 1:
+										$triagecolor = "label-danger";
+										$triagename = "Immediate";
+										break;
+									case 2:
+										$triagecolor = "label-warning";
+										$triagename = "Delayed";
+										break;
+									case 3:
+										$triagecolor = "label-success";
+										$triagename = "Minor";
+										break;
+									case 4:
+										$triagecolor = "label-default";
+										$triagename = "Deceased";
+										break;
+								}
+								echo "
+									<tr>
+										<td><span class='label ".$triagecolor."'>".$triagename."</span> ".$row["firstName"]." ".$row["lastName"]." (".$row["age"]."M)</td>
+										<td>".$row["chiefComplaint"]."</td>
+										<td>".$row["status"]."</td>
+									</tr>
+								";
+							}
+						}
+						$conn->close();
+echo "				</tbody>
+				</table>";
+			} else {
+				echo "<center>
+						<h4>No Assigned Incident</h4>
+					</center>";
+			}
+echo"		</div>
 		</div>
 	</div>
 
     <div class='col-sm-7'>
 		<div class='panel panel-default'>
 			<div class='panel-heading'>Triage</div>
-				<div class='panel-body'>
+				<div class='panel-body'>";
+					if ($_SESSION["incident"] != null) {
+						$deceased = 0;
+						$unknown = 0;
+						$minor = 0;
+						$delayed = 0;
+						$immediate = 0;
+						$conn = new mysqli($db_server, $db_user, $db_password, $db_db);
+						$sql = "SELECT SUM(CASE WHEN triage = 4 THEN 1 ELSE 0 END) AS deceased, SUM(CASE WHEN triage = 0 THEN 1 ELSE 0 END) AS unknown, SUM(CASE WHEN triage = 1 THEN 1 ELSE 0 END) AS immediate, SUM(CASE WHEN triage = 2 THEN 1 ELSE 0 END) AS yellow, SUM(CASE WHEN triage = 3 THEN 1 ELSE 0 END) AS minor FROM patients WHERE Incident = ".$_SESSION["incident"];
+						$result = $conn->query($sql);
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								$deceased = $row["deceased"];
+								$unknown = $row["unknown"];
+								$minor = $row["minor"];
+								$delayed = $row["yellow"];
+								$immediate = $row["immediate"];
+							}
+						}
+						$conn->close();
+						echo "
 					<ul class='list-group'>
 						<div class='col-sm-6'>
-							<li class='list-group-item list-group-item-danger'>Immediate <span class='badge'>12</span></li>
-							<li class='list-group-item list-group-item-warning'>Urgent <span class='badge'>12</span></li>
-							<li class='list-group-item list-group-item-success'>Delayed <span class='badge'>12</span></li>
+							<li class='list-group-item list-group-item-danger'>Immediate <span class='badge'>".$immediate."</span></li>
+							<li class='list-group-item list-group-item-warning'>Delayed <span class='badge'>".$delayed."</span></li>
+							<li class='list-group-item list-group-item-success'>Minor <span class='badge'>".$minor."</span></li>
 						</div>
 						<div class='col-sm-6'>				
-							<li class='list-group-item'>Deceased/Expectant <span class='badge'>12</span></li>
-							<li class='list-group-item active'>Unknown <span class='badge'>12</span></li>
+							<li class='list-group-item'>Deceased/Expectant <span class='badge'>".$deceased."</span></li>
+							<li class='list-group-item active'>Unknown <span class='badge'>".$unknown."</span></li>
 						</div>
-					</ul>
-				</div>
-		</div>
+					</ul>";
+					} else {
+						echo "<center>
+							<h4>No Assigned Incident</h4>
+						</center>";
+					}
+echo "			</div>
+			</div>
 		<div class='panel panel-default'>
 			<div class='panel-heading'>Hospitals</div>
 			<div class='panel-body'>
@@ -53,7 +114,7 @@ echo"
 							<th>Name</th>
 							<th>Distance</th>
 							<th>Diversion</th>
-							<th>Type</th>
+							<th>Capabilities</th>
 						</tr>
 					</thead>
 					<tbody>";
@@ -118,6 +179,7 @@ echo"
 						</tr>";
 							}
 						}
+						$conn->close();
 echo"					</tbody>
 				</table>
 			</div>
