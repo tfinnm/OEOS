f490b66f350647de097479121e37a034e52a3053
Toby McDonald <toby_mcdonald@email.com>
2022-03-16 11:58:05 -0400

    Migrating Libraries

--- header.php
+++ header.php
@@ -1,4 +1,28 @@
 <?php
+function bootlibs() {
+	$termMode=false;
+	if ($termMode) {
+	print "
+		<meta charset=\"utf-8\">
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+		<link rel=\"stylesheet\" href=\"libraries/bootstrap-386-dist/css/bootstrap.min.css\">
+		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+		<script src=\"libraries/bootstrap-386-dist/js/bootstrap.min.js\"></script>
+		<script src=\"libraries/dialogs/js/bootstrap-dialog.min.js\"></script>
+		<link rel=\"stylesheet\" href=\"libraries/dialogs/css/bootstrap-dialog.min.css\">
+	";
+	} else {
+		print "
+		<meta charset=\"utf-8\">
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+		<link rel=\"stylesheet\" href=\"libraries/bootstrap-3.4.1-dist/css/bootstrap.min.css\">
+		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+		<script src=\"libraries/bootstrap-3.4.1-dist/js/bootstrap.min.js\"></script>
+		<script src=\"libraries/dialogs/js/bootstrap-dialog.min.js\"></script>
+		<link rel=\"stylesheet\" href=\"libraries/dialogs/css/bootstrap-dialog.min.css\">
+	";
+	}
+}
 function topbar($admin = false) {
 	session_start();
 	include("db.php");
@@ -26,15 +50,7 @@ function topbar($admin = false) {
 		$conn->close();
 	}
 		
-	print "
-		<meta charset=\"utf-8\">
-		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-		<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">
-		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
-		<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\"></script>
-		<script src=\"libraries/dialogs/js/bootstrap-dialog.min.js\"></script>
-		<link rel=\"stylesheet\" href=\"libraries/dialogs/css/bootstrap-dialog.min.css\">
-	";
+	bootlibs();
 	
 	print "
 		<nav class=\"navbar navbar-inverse\">
